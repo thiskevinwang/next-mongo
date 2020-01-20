0b81eb7ae6f64a16cfb7112047a3e6146c54bfd3
Kevin Wang <kwangsan@gmail.com>
2020-01-20 02:02:17 -0500

    EventById : loading + logic cleanup

--- src/pages/events/[id].tsx
+++ src/pages/events/[id].tsx
@@ -15,68 +15,63 @@ const EventById: NextPage = () => {
     url => fetch(url).then(r => r.json())
   )
 
-  if (!event) return <>"EventLoadinng"</>
+  if (!event) return <>Loading</>
   if (error) return <>Error</>
 
   return (
     <Layout title={"Event Details"}>
       <div>
-        {event ? (
-          <div>
-            <h1>Event Details</h1>
-            <h2>Name</h2>
+        <h1>Event Details</h1>
+        <h2>Name</h2>
+        <code
+          style={{
+            marginLeft: `1rem`,
+            marginRight: `1rem`,
+            display: `inline-block`,
+          }}
+        >
+          {event.name}
+        </code>
+
+        <h2>Description</h2>
+        <p>{event.description}</p>
+        <div
+          style={{
+            marginTop: `1rem`,
+            marginBottom: `1rem`,
+          }}
+        >
+          <h2>Properties</h2>
+          {event.properties?.map((e, i) => (
             <code
+              key={i}
               style={{
                 marginLeft: `1rem`,
                 marginRight: `1rem`,
                 display: `inline-block`,
               }}
             >
-              {event.name}
+              {e}
             </code>
-
-            <h2>Description</h2>
-            <p>{event.description}</p>
-            <div
+          ))}
+        </div>
+        <div style={{ marginTop: `1rem`, marginBottom: `1rem` }}>
+          <h2>Platforms</h2>
+          {event.platforms?.map((e, i) => (
+            <code
+              key={i}
               style={{
-                marginTop: `1rem`,
-                marginBottom: `1rem`,
+                marginLeft: `1rem`,
+                marginRight: `1rem`,
+                display: `inline-block`,
               }}
             >
-              <h2>Properties</h2>
-              {event.properties?.map((e, i) => (
-                <code
-                  key={i}
-                  style={{
-                    marginLeft: `1rem`,
-                    marginRight: `1rem`,
-                    display: `inline-block`,
-                  }}
-                >
-                  {e}
-                </code>
-              ))}
-            </div>
-            <div style={{ marginTop: `1rem`, marginBottom: `1rem` }}>
-              <h2>Platforms</h2>
-              {event.platforms?.map((e, i) => (
-                <code
-                  key={i}
-                  style={{
-                    marginLeft: `1rem`,
-                    marginRight: `1rem`,
-                    display: `inline-block`,
-                  }}
-                >
-                  {e}
-                </code>
-              ))}
-            </div>
-          </div>
-        ) : (
-          <>Event not found</>
-        )}
+              {e}
+            </code>
+          ))}
+        </div>
       </div>
+
       <Link href="/events/[id]/edit" as={`/events/${id}/edit`}>
         <a>Edit</a>
       </Link>
